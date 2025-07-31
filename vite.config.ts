364bb9da3738b7d259cf12acc9c734b749f8657a
Joy <joykarmakar987654321@gmail.com>
2025-07-31 13:48:06 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API_URL = process.env.API_URL!;
+const domain = new URL(API_URL).hostname;
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: API_URL,
+				changeOrigin: true,
+				cookieDomainRewrite: domain,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	}
 });
